1ce22fa60b9702c9824207ad34a62ff37336bc84
Farzeen-qzx <farzeenqzx@gmail.com>
2026-08-18 15:00:30 +0530

    Time: 0 ms (100%), Space: 62.3 MB (19.04%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.js
@@ -0,0 +1,10 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    result=s.reverse()
+  return result;
+
+};
+console.log(reverseString(["h","e","l","l","o"]));
